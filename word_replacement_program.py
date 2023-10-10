305a2cff917921cbede2f2e026b78f6e09cbca5f
demo-23home <zeyadslama23@gmail.com>
2023-10-11 01:30:12 +0300

    simple word_replacement_program

---
+++ word_replacement_program.py
@@ -0,0 +1,8 @@
+def replace_word():
+    str = input("enter your stirng: ")
+    word_to_replace = input("enter a Word to replace: ")
+    word_replacement = input("enter the word replacement: ")
+    print(str.replace(word_to_replace, word_replacement))
+
+
+replace_word()
